83ffa2eaecd7cf2841d3486d415baace8b462d4e
hesauhugo <hesau.hugo@hotmail.com>
2023-05-07 20:38:12 -0300

    feat: value tuple

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿
-(int,string,string,decimal) tupla = (1,"Enzo","Gabriel",1.80M);
 
+ValueTuple<int,string,string,decimal> tupla = (1,"Enzo","Gabriel",1.80M);
 Console.WriteLine($"Id: {tupla.Item1}");
 Console.WriteLine($"Nome: {tupla.Item2}");
 Console.WriteLine($"SobreNome: {tupla.Item3}");
 Console.WriteLine($"Altura: {tupla.Item4}");
+
